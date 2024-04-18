d7defaa759f78b53ef951cbfe3ae7f595b0cc8d9
airu3 <sugar.tom5310@gmail.com>
2024-04-18 17:42:05 +0900

    TryParseメソッドが存在しない場合にNotSupportedExceptionをスローするように変更

--- obj/Debug/net8.0/SystemHelper.AssemblyInfo.cs
+++ obj/Debug/net8.0/SystemHelper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4db3d0503c16dc98ceea87c2db949358aa9781")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
