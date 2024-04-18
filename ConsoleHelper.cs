69f579aa78b1bc14c63f42fac3a49e94cfbfc4a4
airu3 <sugar.tom5310@gmail.com>
2024-04-18 17:41:56 +0900

    TryParseメソッドが存在しない場合にNotSupportedExceptionをスローするように変更

--- ConsoleHelper.cs
+++ ConsoleHelper.cs
@@ -5,21 +5,25 @@ namespace SystemHelper
 {
 	public static class ConsoleHelper
 	{
-		public static T ReadLineAs<T>(string message)
+		public static T ReadLineAs<T>(string? message)
 		{
 			var tryParseMethod = typeof(T).GetMethod("TryParse", [typeof(string), typeof(T).MakeByRefType()])
-			?? throw new NotSupportedException($"Type {typeof(T)} does not have a TryParse method.");
-
+				?? throw new NotSupportedException($"Type {typeof(T)} does not have a TryParse method.");
 
 			T result = default!;
 			string input;
+			object?[] parameters = new object[2];
 
 			do
 			{
 				Console.Write(message);
 				input = Console.ReadLine()!;
+				parameters[0] = input;
+				parameters[1] = result;
 			}
-			while (!(bool)tryParseMethod.Invoke(null, [input, result])!);
+			while (!(bool)tryParseMethod.Invoke(null, parameters)!);
+
+			result = (T)parameters[1]!;
 
 			return result;
 		}
