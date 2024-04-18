768bb1ece8985aaef4669281dde2b30ff685c304
airu3 <sugar.tom5310@gmail.com>
2024-04-18 15:54:14 +0900

    init

---
+++ ConsoleHelper.cs
@@ -0,0 +1,30 @@
+﻿// 人のあらゆる間違いパターンを網羅するようにプログラムを作成すること
+using System;
+
+namespace SystemHelper
+{
+	public static class ConsoleHelper
+	{
+		public static T ReadLineAs<T>(string message)
+		{
+			var tryParseMethod = typeof(T).GetMethod("TryParse", new[] { typeof(string), typeof(T).MakeByRefType() });
+
+			if (tryParseMethod == null)
+			{
+				throw new NotSupportedException($"Type {typeof(T)} does not have a TryParse method.");
+			}
+
+			T result = default;
+			string input;
+
+			do
+			{
+				Console.Write(message);
+				input = Console.ReadLine() ?? string.Empty;
+			}
+			while (!(bool)tryParseMethod.Invoke(null, new object[] { input, result }));
+
+			return result;
+		}
+	}
+}
