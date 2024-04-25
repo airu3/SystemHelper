4653b8821f1101f458b586e62a91d4a9d60b1c80
airu3 <sugar.tom5310@gmail.com>
2024-04-25 16:34:39 +0900

    TryParseメソッドが存在しない場合にNotSupportedExceptionをスローするように変更

--- ConsoleHelper.cs
+++ ConsoleHelper.cs
@@ -5,25 +5,20 @@ namespace SystemHelper
 {
 	public static class ConsoleHelper
 	{
-		public static T ReadLineAs<T>(string? message)
+		public static T ReadLineAs<T>(string message)
 		{
 			var tryParseMethod = typeof(T).GetMethod("TryParse", [typeof(string), typeof(T).MakeByRefType()])
-				?? throw new NotSupportedException($"Type {typeof(T)} does not have a TryParse method.");
+			?? throw new NotSupportedException($"Type {typeof(T)} does not have a TryParse method.");
 
 			T result = default!;
 			string input;
-			object?[] parameters = new object[2];
 
 			do
 			{
 				Console.Write(message);
 				input = Console.ReadLine()!;
-				parameters[0] = input;
-				parameters[1] = result;
 			}
-			while (!(bool)tryParseMethod.Invoke(null, parameters)!);
-
-			result = (T)parameters[1]!;
+			while (!(bool)tryParseMethod.Invoke(null, [input, result])!);
 
 			return result;
 		}
--- obj/Debug/net8.0/SystemHelper.AssemblyInfo.cs
+++ obj/Debug/net8.0/SystemHelper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4db3d0503c16dc98ceea87c2db949358aa9781")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7defaa759f78b53ef951cbfe3ae7f595b0cc8d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
